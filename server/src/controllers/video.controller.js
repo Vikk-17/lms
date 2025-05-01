170bcf630201124b0f83102a9c99a1104be8e74e
sumit devs <mysumitdevs@gmail.com>
2025-05-02 01:26:10 +0530

    Implement CRUD operations for courses, modules, and videos;

--- server/src/controllers/video.controller.js
+++ server/src/controllers/video.controller.js
@@ -1,20 +1,60 @@
-export const getVideosByModule = async (req,res)=>{
-    res.status(200).json({message:"hello from all getVideosByModule"});
+import { 
+    findVideoByModule,
+    insertVideoIntoModule,
+    findVideoById,
+    updateVideoById,
+    deleteVideoById
+ } from "../services/db/video.service.js";
+
+ export const getVideosByModule = async (req,res)=>{
+    try{
+        const moduleId = req.params.moduleid;
+        const videos = await findVideoByModule(moduleId);
+        res.status(200).json(videos);
+    } catch(error){
+        res.status(500).json({message:'internal error',error});
+    }
 };
 
 export const createVideoToModule = async (req,res)=>{
-    res.status(200).json({message:"hello from all createVideoToModule"});
+    try{
+        const videoData = req.body;
+        const moduleId = req.params.moduleid;
+        const newVideo = await insertVideoIntoModule({...videoData,module:moduleId});
+        res.status(200).json(newVideo);
+    } catch(error){
+        res.status(500).json({message:'internal error',error});
+    }
 };
 
 export const getVideo = async (req,res)=>{
-    res.status(200).json({message:"hello from all getVideo"});
+    try{
+        const videoId = req.params.videoid;
+        const video = await findVideoById(videoId);
+        res.status(200).json(video);
+    } catch(error){
+        res.status(500).json({message:'internal error',error});
+    }
 };
 
 export const updateVideo = async (req,res)=>{
-    res.status(200).json({message:"hello from all updateVideo"});
+    try{
+        const videoId = req.params.videoid;
+        const videoData = req.body;
+        const udatedVideo = await updateVideoById(videoId,videoData);
+        res.status(200).json(udatedVideo);
+    } catch(error){
+        res.status(500).json({message:'internal error',error});
+    }
 };
 
 export const deleteVideo = async (req,res)=>{
-    res.status(200).json({message:"hello from all deleteVideo"});
+   try{
+    const videoId = req.params.videoid;
+    const deletedVideo = await deleteVideoById(videoId);
+    res.status(200).json(deletedVideo);
+   } catch(error){
+    res.status(500).json({message:'internal error',error});
+   }
 };
 
